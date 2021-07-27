e7f248424e634d5360dda28afa7cff106f00c4e0
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-27 20:33:05 +0200

    Add type definition for Finder

--- src/str-is-in.js
+++ src/str-is-in.js
@@ -9,6 +9,12 @@ const { insensitiveMatchFull, insensitiveMatchPartial } = require('./comparators
  */
 
 /**
+ * @typedef Finder
+ * @property {(value: string, entries: string[]) => string} full Performs a case-insensitive and full-matching search for a given value inside an array of values and returns the found match (with the original case being preserved). If none is found an empty string is returned.
+ * @property {(value: string, entries: string[]) => string} partial Performs a case-insensitive and partial-matching search for a given value inside an array of values and returns the found match (with the original case being preserved). If none is found an empty string is returned.
+ */
+
+/**
  * Checks whether the given String is present
  * in the provided array of Strings, full-matching, case-sensitive.
  *@public
@@ -38,18 +44,9 @@ function strIsIn(value, entries, comparator) {
 }
 
 /**
- * Provides methods for finding matches.
+ * @type {Finder} Provides methods for finding matches.
  */
 const findMatch = {
-  /**
-   * Performs a case-insensitive and full-matching search for a given value
-   * inside an array of values and returns the found match
-   * (with the original case being preserved). If none is found
-   * an empty string is returned.
-   * @param {string} value
-   * @param {string[]} entries
-   * @returns {string}
-   */
   full: (value, entries) => {
     if (!value || !entries || !(entries instanceof Array)) {
       return ''
@@ -65,15 +62,6 @@ const findMatch = {
 
     return ''
   },
-  /**
-   * Performs a case-insensitive and partial-matching search for a given value
-   * inside an array of values and returns the found match
-   * (with the original case being preserved). If none is found
-   * an empty string is returned.
-   * @param {string} value
-   * @param {string[]} entries
-   * @returns {string}
-   */
   partial: (value, entries) => {
     if (!value || !entries || !(entries instanceof Array)) {
       return ''
