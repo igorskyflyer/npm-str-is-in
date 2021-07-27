efa9479dbeef604737d5b31d40f9f358158e6040
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-27 19:33:49 +0200

    Search for a String and return its value upon match
    Fixes:
    - add 2 new function for match-finding,
    - rename main entry point.
    
    closes #3

---
+++ src/index.js
@@ -0,0 +1,96 @@
+const { sensitiveMatchFull } = require('./comparators/sensitiveMatch')
+const { insensitiveMatchFull, insensitiveMatchPartial } = require('./comparators/insensitiveMatch')
+
+/**
+ * @callback ComparatorCallback
+ * @param {string} entry
+ * @param {string} value
+ * @returns {boolean}
+ */
+
+/**
+ * Checks whether the given String is present
+ * in the provided array of Strings, full-matching, case-sensitive.
+ *@public
+ * @param {string} value
+ * @param {string[]} entries
+ * @param {ComparatorCallback} [comparator]
+ * @returns {boolean}
+ */
+function strIsIn(value, entries, comparator) {
+  if (!value || !entries || !(entries instanceof Array)) {
+    return false
+  }
+
+  const count = entries.length
+
+  if (typeof comparator !== 'function') {
+    comparator = sensitiveMatchFull
+  }
+
+  for (let i = 0; i < count; i++) {
+    if (comparator(entries[i], value)) {
+      return true
+    }
+  }
+
+  return false
+}
+
+/**
+ * Performs a case-insensitive and full-matching search for a given value
+ * inside an array of values and returns the found match
+ * (with the original case being preserved). If none is found
+ * an empty string is returned.
+ *@public
+ * @param {string} value
+ * @param {string[]} entries
+ * @returns {string}
+ */
+function findMatch(value, entries) {
+  if (!value || !entries || !(entries instanceof Array)) {
+    return ''
+  }
+
+  const count = entries.length
+
+  for (let i = 0; i < count; i++) {
+    if (insensitiveMatchFull(entries[i], value)) {
+      return entries[i]
+    }
+  }
+
+  return ''
+}
+
+/**
+ * Performs a case-insensitive and partial-matching search for a given value
+ * inside an array of values and returns the found match
+ * (with the original case being preserved). If none is found
+ * an empty string is returned.
+ *@public
+ * @param {string} value
+ * @param {string[]} entries
+ * @returns {string}
+ */
+function findMatchPartial(value, entries) {
+  if (!value || !entries || !(entries instanceof Array)) {
+    return ''
+  }
+
+  const count = entries.length
+
+  for (let i = 0; i < count; i++) {
+    if (insensitiveMatchPartial(entries[i], value)) {
+      return entries[i]
+    }
+  }
+
+  return ''
+}
+
+module.exports = {
+  strIsIn,
+  findMatch,
+  findMatchPartial,
+}
