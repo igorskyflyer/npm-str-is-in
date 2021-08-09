dd3557fb010ae76e53589559fd13f1a62deaaf54
igorskyflyer <igor.dvlpr@gmail.com>
2021-08-09 16:07:23 +0200

    Update Comparator

--- src/comparators/insensitiveMatch.js
+++ src/comparators/insensitiveMatch.js
@@ -3,11 +3,13 @@
  * @param {string} value
  * @returns {boolean}
  */
-export const insensitiveMatchFull = (entry, value) => entry.toLowerCase() === value.toLowerCase()
+const insensitiveMatchFull = (entry, value) => entry.toLowerCase() === value.toLowerCase()
 
 /**
  *  @param {string} entry
  * @param {string} value
  * @returns {boolean}
  */
-export const insensitiveMatchPartial = (entry, value) => entry.toLowerCase().indexOf(value.toLowerCase()) > -1
+const insensitiveMatchPartial = (entry, value) => entry.toLowerCase().indexOf(value.toLowerCase()) > -1
+
+module.exports = { insensitiveMatchFull, insensitiveMatchPartial }
