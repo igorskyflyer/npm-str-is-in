d218b7d78689fd51d7c84ec852c8fec9df38c7c8
igorskyflyer <igor.dvlpr@gmail.com>
2021-08-15 00:12:48 +0200

    Convert to ESM syntax

--- src/comparators/insensitiveMatch.js
+++ src/comparators/insensitiveMatch.js
@@ -3,13 +3,11 @@
  * @param {string} value
  * @returns {boolean}
  */
-const insensitiveMatchFull = (entry, value) => entry.toLowerCase() === value.toLowerCase()
+export const insensitiveMatchFull = (entry, value) => entry.toLowerCase() === value.toLowerCase()
 
 /**
  *  @param {string} entry
  * @param {string} value
  * @returns {boolean}
  */
-const insensitiveMatchPartial = (entry, value) => entry.toLowerCase().indexOf(value.toLowerCase()) > -1
-
-module.exports = { insensitiveMatchFull, insensitiveMatchPartial }
+export const insensitiveMatchPartial = (entry, value) => entry.toLowerCase().indexOf(value.toLowerCase()) > -1
